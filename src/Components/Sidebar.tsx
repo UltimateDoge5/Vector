91c72fce93d1d534d0fd0fa670df2477b4fb143f
Marcin <kimbergrav2@gmail.com>
2023-10-18 15:59:32 +0200

    Added teacher/admin sidebar

--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -1,9 +1,14 @@
 import Link from "next/link"
-import {HomeIcon, AcademicCapIcon, CheckBadgeIcon, TableCellsIcon, PencilSquareIcon} from '@heroicons/react/24/outline'
+import {HomeIcon, AcademicCapIcon, CheckBadgeIcon, TableCellsIcon, PencilSquareIcon, UsersIcon} from '@heroicons/react/24/outline'
 import { JSX } from "react";
-import { UrlObject } from "url";
+import { currentUser } from "@clerk/nextjs";
+import DisclosureButton from "./Disclosure";
 
-export default function Sidebar(){
+export default async function Sidebar(){
+    const user = await currentUser();
+
+    const role = user?.privateMetadata.role ?? "student";
+
     return (
         <aside className="w-[300px] px-5">
             <div className="flex flex-col gap-y-1">
@@ -12,8 +17,15 @@ export default function Sidebar(){
                 <SidebarItem name="Frekwencja" icon={<CheckBadgeIcon className="h-5 w-5"/>} link="/presence"/>
                 <SidebarItem name="Plan zajęć" icon={<TableCellsIcon className="h-5 w-5"/>} link="/schedule"/>
                 <SidebarItem name="Zadania" icon={<PencilSquareIcon className="h-5 w-5"/>} link="/assignments"/>
-
             </div>
+            {
+                role != "student" ? (
+                    <div className="flex flex-col gap-y-1 border-t-2 pt-2 mt-2">
+                        <DisclosureButton name="Użytkownicy" icon={<UsersIcon className="h-5 w-5"/>} subNames={[{name: "Uczniowie", link: "/students"}, {name: "Nauczyciele", link: "/teachers"}]} />
+                    </div>
+                ) : (<></>)
+            }
+            
         </aside>
     );
 }
