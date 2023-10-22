b8522695cc94aa90acc3849f6ad366885a9dcc49
Kacper Pachocki <kacperpachocki04@gmail.com>
2023-10-22 16:31:29 +0200

    Added components folder

---
+++ src/components/students/studentsManagement.tsx
@@ -0,0 +1,183 @@
+"use client"
+
+import { Menu, Transition } from "@headlessui/react";
+import { ArchiveBoxXMarkIcon, EllipsisVerticalIcon, PencilSquareIcon } from "@heroicons/react/24/outline";
+import { ColumnDef } from "@tanstack/react-table";
+import { Fragment, useState } from "react";
+import { ClassDto, StudentDto } from "~/types/dtos";
+import { DataTable } from "../dataTable";
+import AddStudentModal from "./addStudentModal";
+import EditStudentModal from "./editStudentModal";
+
+interface Props {
+    students: StudentDto[],
+    classes: ClassDto[],
+}
+
+interface EditModalState {
+    student: StudentDto,
+    isOpen: boolean
+}
+
+export default function StudentsManagement({ students, classes }: Props) {
+    const [studentsList, setStudentsList] = useState(students);
+
+    const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+    const [editModalState, setEditModalState] = useState<EditModalState>({ student: {}, isOpen: false });
+
+    const addStudent = async (formData: { name: string, email: string }, classId: number) => {
+        const payload = {
+            firstName: formData.name.split(" ")[0],
+            lastName: formData.name.split(" ")[1],
+            email: formData.email,
+            classId: classId
+        };
+
+        const response = await fetch("/students/api", {
+            method: "POST",
+            body: JSON.stringify(payload)
+        });
+
+        if (response.ok) {
+            const studentWithPassword = await response.json();
+
+            console.log(`Domyślne hasło: ${studentWithPassword.password}`);
+
+            setStudentsList([...studentsList, studentWithPassword]);
+        }
+    }
+
+    const editStudent = async (userId: string, name: string, classId: number) => {
+        const payload = {
+            userId,
+            firstName: name.split(" ")[0],
+            lastName: name.split(" ")[1],
+            classId
+        };
+
+        const response = await fetch("/students/api", {
+            method: "PUT",
+            body: JSON.stringify(payload)
+        });
+
+        if (response.ok) {
+            console.log(response);
+
+            const updatedClass = classes.find(clas => clas.id === classId);
+
+            setStudentsList(studentsList.map(student => student.userId === userId ? { ...student, name, class: updatedClass } : student))
+        }
+    }
+
+    const deleteStudent = async (userId: string) => {
+        const response = await fetch("/students/api", {
+            method: "DELETE",
+            body: JSON.stringify({ userId })
+        });
+
+        if (response.ok) {
+            console.log(response);
+            setStudentsList(studentsList.filter(student => student.userId != userId));
+        }
+    }
+
+    const columns: ColumnDef<StudentDto>[] = [
+        {
+            header: "Imię i Nazwisko",
+            accessorKey: "name",
+        },
+        {
+            header: "Klasa",
+            accessorKey: "class.name",
+        },
+        {
+            header: " ",
+            accessorKey: "",
+            cell: ({ row }) => (
+                <Menu as="div" className="relative inline-block text-right w-full">
+                    <div>
+                        <Menu.Button className="inline-flex w-full justify-end rounded-3x px-2 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+                            <EllipsisVerticalIcon className="h-5 w-5 text-accent/70" />
+                        </Menu.Button>
+                    </div>
+                    <Transition
+                        as={Fragment}
+                        enter="transition ease-out duration-100"
+                        enterFrom="transform opacity-0 scale-95"
+                        enterTo="transform opacity-100 scale-100"
+                        leave="transition ease-in duration-75"
+                        leaveFrom="transform opacity-100 scale-100"
+                        leaveTo="transform opacity-0 scale-95"
+                    >
+                        <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                            <div className="px-1 py-1" onClick={() => setEditModalState({ student: row.original, isOpen: true })}>
+                                <Menu.Item>
+                                    {({ active }) => (
+                                        <button
+                                            className={`${active ? 'bg-accent/50 text-white' : 'text-text'
+                                                } group flex w-full items-center rounded-md px-2 py-2 text-sm font-bold`}
+                                        >
+                                            <PencilSquareIcon
+                                                className="mr-2 h-5 w-5"
+                                                aria-hidden="true"
+                                            />
+                                            Edytuj
+                                        </button>
+                                    )}
+                                </Menu.Item>
+                            </div>
+
+                            <div className="px-1 py-1 " onClick={() => deleteStudent(row.original.userId)}>
+                                <Menu.Item>
+                                    {({ active }) => (
+                                        <button
+                                            className={`${active ? 'bg-accent/50 text-white' : 'text-text'
+                                                } group flex w-full items-center rounded-md px-2 py-2 text-sm font-bold`}
+                                        >
+                                            <ArchiveBoxXMarkIcon
+                                                className="mr-2 h-5 w-5"
+                                                aria-hidden="true"
+                                            />
+                                            Usuń
+                                        </button>
+                                    )}
+                                </Menu.Item>
+                            </div>
+
+                        </Menu.Items>
+                    </Transition>
+                </Menu>
+            )
+        }
+    ];
+
+
+    return (
+        <div className="px-10">
+
+            <DataTable
+                columns={columns}
+                data={studentsList}
+                noDataText="Brak uczniów"
+                title="Uczniowie"
+                primaryActionBtn={<button className="bg-primary hover:bg-primary/90 py-3 px-5 rounded-lg text-text font-bold " onClick={() => setIsAddModalOpen(true)}>Dodaj ucznia</button>}
+            />
+
+            <AddStudentModal
+                isOpen={isAddModalOpen}
+                setIsOpen={setIsAddModalOpen}
+                addStudent={addStudent}
+                classes={classes}
+            />
+
+            <EditStudentModal
+                student={editModalState.student}
+                isOpen={editModalState.isOpen}
+                setIsOpen={(state: boolean) => setEditModalState({ ...editModalState, isOpen: state })}
+                editStudent={editStudent}
+                classes={classes}
+            />
+
+        </div>
+    )
+}
---
+++ src/components/teachers/editTeacherModal.tsx
@@ -0,0 +1,87 @@
+"use client"
+
+import { Dialog, Transition } from '@headlessui/react';
+import { FormEvent, Fragment, useEffect, useState } from 'react';
+import { TeacherDto } from '~/types/dtos';
+
+interface Props {
+    teacher: TeacherDto
+    isOpen: boolean,
+    setIsOpen: (state: boolean) => void,
+    editTeacher: (userId: string, name: string) => void
+}
+
+export default function EditTeacherModal({ teacher, isOpen, setIsOpen, editTeacher }: Props) {
+    const [name, setName] = useState(teacher.name);
+
+    useEffect(() => {
+        setName(teacher.name);
+    }, [teacher])
+
+    const onSubmit = (e: FormEvent) => {
+        e.preventDefault();
+
+        if (!name) {
+            alert("Wypełnij poprawnie formularz");
+            return;
+        }
+
+        editTeacher(teacher.userId, name);
+        setIsOpen(false);
+    }
+
+
+    return (
+        <Transition appear show={isOpen} as={Fragment}>
+            <Dialog as="div" className="relative z-10" onClose={() => setIsOpen(false)}>
+                <Transition.Child
+                    as={Fragment}
+                    enter="ease-out duration-300"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="ease-in duration-200"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                >
+                    <div className="fixed inset-0 bg-black bg-opacity-25" />
+                </Transition.Child>
+
+                <div className="fixed inset-0 overflow-y-auto">
+                    <div className="flex min-h-full items-center justify-center p-4 text-center">
+                        <Transition.Child
+                            as={Fragment}
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0 scale-95"
+                            enterTo="opacity-100 scale-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100 scale-100"
+                            leaveTo="opacity-0 scale-95"
+                        >
+                            <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                                <Dialog.Title
+                                    as="h3"
+                                    className="text-lg font-bold leading-6 text-gray-900"
+                                >
+                                    Edytuj {teacher.name}
+                                </Dialog.Title>
+
+                                <form className="flex flex-col my-3" onSubmit={onSubmit}>
+                                    <input
+                                        type="text"
+                                        className="w-full p-4 bg-secondary/30 rounded-lg outline-none text-text flex-1 my-2"
+                                        placeholder="Imię i Nazwisko"
+                                        value={name}
+                                        onChange={(e) => setName(e.target.value)}
+                                    />
+
+                                    <button className="bg-primary hover:bg-primary/90 py-3 rounded-lg text-text font-bold mt-2 disabled:bg-primary/50 disabled:cursor-not-allowed transition" type="submit" disabled={!name}>Edytuj</button>
+                                </form>
+
+                            </Dialog.Panel>
+                        </Transition.Child>
+                    </div>
+                </div>
+            </Dialog>
+        </Transition>
+    )
+}
