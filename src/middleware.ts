9266d42d45bcd93fa2fe4d6e1e67d6b1b82abf0e
Kacper Pachocki <kacperpachocki04@gmail.com>
2023-10-23 23:21:21 +0200

    Added admin auth middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,37 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { authMiddleware, clerkClient, redirectToSignIn } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+import { UserType } from "./enums/UserType";
 
-export default authMiddleware();
+const adminPaths = ["/teachers", "/students*"];
+ 
+const isAdminPath = (path: string) => {
+  return adminPaths.find((x) =>
+    path.match(new RegExp(`^${x}$`.replace("*$", "($|/)")))
+  );
+};
+
+export default authMiddleware({
+  publicRoutes: [],
+  afterAuth(auth, req, evt) {
+
+    if (!auth.userId && !auth.isPublicRoute) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+
+    if(isAdminPath(req.nextUrl.pathname)) {
+        return clerkClient.users.getUser(auth.userId!)
+        .then(user => {
+          if(user.privateMetadata.role != UserType.ADMIN) {
+            return NextResponse.redirect(new URL("/", req.url));
+          }
+
+          return NextResponse.next();
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+        }).catch(() => redirectToSignIn({ returnBackUrl: req.url }));
+    }
+  }
+});
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api)(.*)"],
