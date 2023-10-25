1d888338dde40dc26db9b8c9b6fd78e615e49d25
Marcin <kimbergrav2@gmail.com>
2023-10-25 20:38:15 +0200

    Fixed UI bug, Changed small issues

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { HomeIcon, AcademicCapIcon, CheckBadgeIcon, TableCellsIcon, PencilSquareIcon, UsersIcon } from "@heroicons/react/24/outline";
+import { HomeIcon, AcademicCapIcon, CheckBadgeIcon, TableCellsIcon, PencilSquareIcon, UsersIcon, Squares2X2Icon } from "@heroicons/react/24/outline";
 import { type JSX } from "react";
 import { currentUser } from "@clerk/nextjs";
 import DisclosureButton from "./disclosure";
@@ -28,6 +28,7 @@ export async function Sidebar() {
                             { name: "Nauczyciele", link: "/teachers" },
                         ]}
                     />
+                    {user?.privateMetadata.role === "admin" ? <SidebarItem name="Klasy" icon={<Squares2X2Icon className="h-5 w-5" />} link="/class" /> : (<></>)}
                 </div>
             ) : (
                 <></>
